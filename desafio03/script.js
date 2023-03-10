629bb61bb33a47102f619c24ceb3f6dcd6d90c04
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-03-10 10:35:24 -0300

    add script update

--- desafio03/script.js
+++ desafio03/script.js
@@ -1,3 +1,5 @@
+
+
 function loading(event){
     const button = event.currentTarget
     button.classList.add('loading')
@@ -7,3 +9,43 @@ function loading(event){
     `
 
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const myButton = document.getElementById("myButton");
+  
+    myButton.addEventListener("click", function() {
+      this.disabled = !this.disabled;
+      if (this.disabled) {
+        this.textContent = "disabled";
+        this.classList.remove("primary");
+        this.classList.add("secondary");
+      } 
+    });
+  });
+  
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const move = document.getElementById("move");
+  
+    move.addEventListener("click", function() {
+      
+        if (this.classList.contains("movable")) {
+          this.classList.remove("movable");
+          this.textContent = "click to move";
+          this.classList.remove("tertiary");
+          this.classList.add("primary");
+          const icon = this.querySelector("i");
+          if (icon) {
+            icon.remove();
+          }
+        } else {
+          this.classList.add("movable");
+          this.textContent = "give up";
+          this.classList.remove("primary");
+          this.classList.add("tertiary");
+          const icon = document.createElement("i");
+          icon.classList.add("ph", "ph-list");
+          this.insertBefore(icon, this.firstChild);
+        }
+    });
+  });
