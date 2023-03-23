b2f81c98f545abd94346b041a533013fa363c19d
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-03-23 17:05:58 -0300

    add new proj

---
+++ 11/scripts.js
@@ -0,0 +1,8 @@
+function togglePassword(){
+  document.querySelectorAll(".eye").forEach((eye) => {
+    eye.classList.toggle("hide")
+  })
+  const type = senha.getAttribute("type") =="password" ?
+  "text" : "password"
+  senha.setAttribute("type", type)
+}
